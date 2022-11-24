7bccafb35fad2a01466108b73a7dd8e5fc0e7b75
Panagiotis Efopoulos <panagiotis@tessera.gr>
2022-11-24 12:33:23 +0200

    initial commit

---
+++ app/src/main/java/com/example/appwithsettings_92/SettingsFragment.java
@@ -0,0 +1,17 @@
+package com.example.appwithsettings_92;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceFragmentCompat;
+
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+
+}
